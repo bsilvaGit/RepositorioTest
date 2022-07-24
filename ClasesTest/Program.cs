a5c3115599dfad2f115ba645b2ac21babc17fba9
bsilvaGit <bsilva@adpconsultoria.com.mx>
2022-07-23 19:01:50 -0500

    Class for Date

---
+++ ClasesTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClasesTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conceptos de POO");
+            Console.WriteLine("================\n");
+
+            try
+            {
+                Console.WriteLine(new Date(2022, 12, 07));
+                Console.WriteLine(new Date(1974, 09, 23));
+                Console.WriteLine(new Date(1985, 11, 15));
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+    }
+}
